5e5c936e11335d40853064458817c8f3f8820fa2
Francisco Solis <fran@theprogramsrc.xyz>
2021-11-06 23:20:06 -0300

    New Features & Cleanup * Moved js to index.js * New Settings * Added Projects * Updated README.md

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Preview
-![](https://i.imgur.com/b2zv3Rg.png)
+![](https://i.imgur.com/7Ua1buQ.png)
 
 # Setup
 1. Make sure to have your [github.io](https://pages.github.com) site
@@ -12,15 +12,44 @@
 - `desc`: A short description for you (Used as variable in the title)
 - `image`: A direct link to your profile image (Used in the site icon and the site links page)
 - `title`: The website title (You may use `{Name}` and `{Desc}` as variables)
-- `theme-color`: The background color for the site
+- `theme-settings`: Set of theme settings
+  - `background-color`: Theme color and site background color
+  - `text-color`: color of all texts on the site
 - `links`: A list with the icons and links.
-
+- `repo`: If you want to use the projects feature fill in this setting, set to `none` or `null` to disable it.
+- `project-list-settings`: Set of settings for the project list
+  - `background-color`: Background color of a single card
+  - `title`: Set of title settings
+    - `color`: Color of the title
+    - `font-size`: Font size of the title
+    - `bold`: true if the title should be bold, false otherwise
+  - `description`: Set of description settings
+    - `color`: Color of the description
+    - `font-size`: Font size of the description
 # Links Format
 ```json
 {"url": "https://example.com/", "display": "fab fa-twitter"}
 ```
 - `url`: The url of the site
 - `display`: The classes for the display icon. Use [fontawesome.com](https://fontawesome.com/) to search for icons, then select the icon and grab the class of it.
+
+# Adding projects to the list
+Once you've linked the repo just create a new issue (must remain open) with the following format:
+```
+body:Body of the project card
+icon:none|direct url
+fontawesomeicon:classes
+url:somerandomurl
+```
+- `title`: The title of the issue will be the title of the card
+- `body`: The body of the project card
+- `icon`: The icon of the project card, set to none or remove at all to remove the icon. You must use the direct url
+- `fontawesomeicon`: The fontawesome icon of the project card, set to none or remove at all to remove the icon. You may add as many fontawesome classes as you want.
+- `url`: The url of the project card, set to none or remove at all to remove the url. This url will be used when someone clicks the card
+
+> Remember to use only the icon _OR_ the fontawesome icon, not both. By default the icon will have priority
+
+You may [check the issues](https://github.com/FranciscoSolisMat/Links/issues) for the project list to see how it works.
 
 # Where to find the FontAwesome class?
 ![](https://i.imgur.com/D8CjZue.png)
---
+++ index.js
@@ -0,0 +1,137 @@
+const setup = (config) => {
+    const setTitle = (title) => document.title = title;
+    const appendMeta = (name, content) => {
+        const meta = document.createElement('meta');
+        meta.setAttribute('name', name);
+        meta.setAttribute('content', content);
+        document.head.appendChild(meta);
+    };
+    const appendLinkRel = (rel, href) => {
+        const link = document.createElement('link');
+        link.setAttribute('rel', rel);
+        link.setAttribute('href', href);
+        document.head.appendChild(link);
+    };
+    const title = config.title.replace('{Name}', config.name).replace('{Desc}', config.desc);
+    setTitle(title);
+    appendMeta('description', config.desc);
+    appendMeta('og:title', title);
+    appendMeta('og:description', config.desc);
+    appendMeta('og:image', config.image);
+    appendMeta('og:secure_image', config.image);
+    appendMeta('og:image:width', '1700');
+    appendMeta('og:image:height', '1700');
+    appendMeta('og:image:type', 'image/png');
+    appendMeta('og:image:alt', config.name);
+    appendMeta('og:url', document.location.href);
+    appendMeta('og:type', 'website');
+    appendMeta('og:site_name', config.name);
+    appendMeta('og:locale', 'es_ES');
+    appendMeta('profile:username', config.name);
+    appendMeta('twitter_title', title);
+    appendMeta('twitter_description', config.desc);
+    appendMeta('twitter_image', config.image);
+    appendMeta('twitter_card', 'summary_large_image');
+    appendMeta('canonical', document.location.href);
+    appendMeta('theme-color', config['theme-settings']['background-color']);
+    appendMeta('msapplication-TileColor', config['theme-settings']['background-color']);
+    appendMeta('msapplication-TileImage', config.image);
+    appendLinkRel('apple-touch-icon', config.image);
+    appendLinkRel('icon', config.image);
+    appendLinkRel('shortcut icon', config.image);
+    document.getElementById('desc').innerText = config.desc;
+    document.getElementById('name').innerText = config.name;
+    const linksEl = document.getElementById('links');
+    config.links.forEach(link => link.id = btoa(link.url));
+    config.links.forEach(link => {
+        const a = document.createElement('a');
+        a.setAttribute('href', link.url);
+        a.setAttribute('target', '_blank');
+        a.setAttribute('rel', 'noopener noreferrer');
+        a.id = link.id;
+        const i = document.createElement('i');
+        link.display.split(' ').forEach(_class => i.classList.add(_class));
+        a.appendChild(i);
+        a.classList.add('mx-2');
+        linksEl.appendChild(a);
+    });
+    config.links.forEach(link => {
+        const child = document.getElementById(link.id).children[0];
+        child.setAttribute('style', "fill: white; pointer-events: none; width: 32px; height: 32px;")
+    });
+    if (config.repo && config.repo !== 'none' && config.repo !== 'null') {
+        loadProjects(config.repo);
+    }
+};
+
+const loadProjects = (repo) => {
+    axios.get(`https://api.github.com/repos/${repo}/issues`).then(res => {
+        if (res.data) {
+            res.data.forEach(item => {
+                const projects = document.getElementById('projects');
+                const div = document.createElement('div');
+                const data = item.body.replaceAll('\n', ':split:').replaceAll('\r', '').replaceAll('\n', '').split(':split:');
+                div.classList.add('flex', 'flex-row', 'items-center', 'p-2', 'w-full', 'rounded-lg', 'transition-all', 'duration-500', 'hover:shadow-2xl', 'cursor-pointer', 'mb-5');
+                div.style.backgroundColor = window.config['project-list-settings']['background-color'];
+                div.id = 'project-' + item.number;
+                const div2 = document.createElement('div');
+                div2.classList.add('flex', 'flex-col')
+                const h1 = document.createElement('h1');
+                if (window.config['project-list-settings']['title']['bold']) { h1.classList.add('font-bold'); }
+                h1.style.fontSize = window.config['project-list-settings']['title']['font-size'];
+                h1.style.color = window.config['project-list-settings']['title']['color'];
+                h1.innerText = item.title;
+                const p = document.createElement('p');
+                p.style.fontSize = window.config['project-list-settings']['description']['font-size'];
+                p.style.color = window.config['project-list-settings']['description']['color'];
+                data.forEach(i2 => {
+                    if (i2.startsWith('body:')) {
+                        p.innerText = i2.replace('body:', '');
+                    } else if (i2.startsWith('url:')) {
+                        div.onclick = () => window.open(i2.replace('url:', ''), '_blank');
+                    }
+                });
+                let added = false
+                data.forEach(i1 => {
+                    if(!added){
+                        if(i1.startsWith('icon:')){
+                            let icon = i1.replace('icon:', '');
+                            if(icon && icon !== 'none' && icon !== 'null'){
+                                const img = document.createElement('img');
+                                img.src = icon
+                                img.classList.add('w-16', 'h-16', 'rounded-full')
+                                div.appendChild(img);
+                                div2.classList.add('ml-4')
+                                added = true;
+                            }
+                        }else if(i1.startsWith('fontawesomeicon:')){
+                            let icon = i1.replace('fontawesomeicon:', '');
+                            if(icon && icon !== 'none' && icon !== 'null'){
+                                const i = document.createElement('i');
+                                i.classList.add('fas', 'fa-5x', 'rounded-full')
+                                icon.split(' ').forEach(_class => i.classList.add(_class));
+                                div.appendChild(i);
+                                div2.classList.add('ml-4')
+                                added = true;
+                            }
+                        }
+                    }
+                });
+                div2.appendChild(h1);
+                div2.appendChild(p);
+                div.appendChild(div2);
+                projects.appendChild(div);
+            });
+        }
+    }).catch(ignored => { });
+};
+
+// Setup using config.json
+axios.get('config.json').then(res => {
+    let config = window.config = res.data;
+    document.querySelectorAll('body')[0].style.backgroundColor = config['theme-settings']['background-color'];
+    ['p', 'h1', 'span', 'div'].forEach(elName => document.querySelectorAll(`${elName}`).forEach(el => el.style.color = config['theme-settings']['text-color']));
+    document.getElementById('icon').src = config.image;
+    document.getElementById('icon').alt = config.name;
+    setup(config)
+});
